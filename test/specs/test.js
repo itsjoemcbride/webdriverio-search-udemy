b18a67ea6ff1981da4dd743bf4119a24621cf68d
Joe <itsjoemcbride@gmail.com>
2022-09-03 00:58:17 +0100

    Modify change,js
    - Runs 5 smaller tests, instead of one larger test
        - If tests break, the breakage is easier to spot

--- test/specs/test.js
+++ test/specs/test.js
@@ -8,39 +8,47 @@
 // Task: Automate the above scenario given using JS and any tool (WebdriverIO or Selenium). The Project should be shared as a GitHub project. Each automation step should have assertions''
 
 describe("Assessment", function () {
-  it("searches google for the udemy course", async function () {
+  it("goes to the Google website and rejects cookies", async function () {
     // 1. Go to google site
     await browser.url("http://www.google.com");
-    let title = await browser.getTitle();
+    const title = await browser.getTitle();
     expect(title).toEqual("Google");
     // Reject cookies
     const cookieBtn = $("div=Reject all");
     await cookieBtn.click();
+  });
 
+  it("searches Google for 'Test Automation Learning'", async function () {
     // 2. Search 'Test Automation Learning'
     const searchBar = $("input");
     await searchBar.click();
     await browser.keys("Test Automation Learning\uE007");
     // Assert that we are on the right page
-    title = await browser.getTitle();
+    const title = await browser.getTitle();
     await expect(title).toContain("Test Automation Learning");
+  });
 
+  it("selects the Google search result which is a Udemy course", async function () {
     // 3. Select the link with Udemy course
     const udemyLink = $("a*=Udemy");
     await udemyLink.click();
     // 4. Assert that we are on the right page
-    title = await browser.getTitle();
+    const title = await browser.getTitle();
     await expect(title).toContain("Udemy");
+  });
 
+  it("searches Udemy for 'BDD with Cucumber'", async function () {
     // 5. Search for BDD with Cucumber
     const udemySearch = $("[role=combobox]");
     await udemySearch.click();
     await udemySearch.keys("BDD with Cucumber\uE007");
     // Assert that we are on the right page
     expect(browser).toHaveUrlContaining("BDD+with+Cucumber");
+  });
 
-    // Sometimes the test halts here as a result of a CAPTCHA on the Udemy website. In reality, testers would usually have access to a bypass API in this scenario. A pause statement was attempted here so that a human could carry out the captcha, but the CAPTCHA would never resolve.
+  // Sometimes the test halts here as a result of a CAPTCHA on the Udemy website. In reality, testers would usually have access to a bypass API in this scenario. A pause statement was attempted here so that a human could carry out the captcha, but the CAPTCHA would never resolve.
 
+  it("sorts the search results by 'Highest Rated' and selects the top result", async function () {
     // 6. Click on the course with highest rating from the list of search results
     const selectBox = await $("input[name='sort']");
     await selectBox.click();
@@ -48,7 +56,7 @@ describe("Assessment", function () {
     const topResult = await $("//h3/a");
     topResult.click();
     // Assert that we have landed on the right page
-    title = await browser.getTitle();
+    const title = await browser.getTitle();
     await expect(title).toContain(
       "Learn to Create BDD Framework using Cucumber and Java"
     );
--- wdio.conf.js
+++ wdio.conf.js
@@ -138,7 +138,7 @@ exports.config = {
   // See the full list at http://mochajs.org/
   mochaOpts: {
     ui: "bdd",
-    timeout: 9999999999999, // default 60000
+    timeout: 60000, // default 60000
   },
   //
   // =====
