3adab1a5479dab29ca3b25ad45787955d6aa4682
Joe <itsjoemcbride@gmail.com>
2022-09-03 01:10:12 +0100

    Refactor test.js  to make step 6 less flaky
    - Added an await action
    - Does not resolve issue RE hCaptcha

--- test/specs/test.js
+++ test/specs/test.js
@@ -50,11 +50,11 @@ describe("Assessment", function () {
 
   it("sorts the search results by 'Highest Rated' and selects the top result", async function () {
     // 6. Click on the course with highest rating from the list of search results
-    const selectBox = await $("input[name='sort']");
+    const selectBox = await $("select[name='sort']");
     await selectBox.click();
     await selectBox.selectByVisibleText("Highest Rated");
     const topResult = await $("//h3/a");
-    topResult.click();
+    await topResult.click();
     // Assert that we have landed on the right page
     const title = await browser.getTitle();
     await expect(title).toContain(
