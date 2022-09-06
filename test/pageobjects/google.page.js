4b903838bf950d0f5065896b7814b76ccc3d3655
Joe <itsjoemcbride@gmail.com>
2022-09-06 21:22:21 +0100

    Refactor google.page.js
    - Change CSS selector for the cookies button for increased stability

--- test/pageobjects/google.page.js
+++ test/pageobjects/google.page.js
@@ -3,8 +3,9 @@ class GooglePage {
     return browser.url("http://www.google.com");
   }
 
+  // While not particularly declarative, W0wltc is the ID on google's "reject all" cookies button, IDs don't often change and are unique, so it was selected as it should be somewhat robust
   get rejectCookiesBtn() {
-    return $("div=Reject all");
+    return $("button#W0wltc");
   }
 
   get searchBar() {
